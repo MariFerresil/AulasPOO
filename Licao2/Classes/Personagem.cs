02cabde56d29ad197400ab1202f05324bc846374
MariFerresil <ferreiramariana95@hotmail.com>
2025-02-08 16:40:38 -0300

    sobe ai

---
+++ Licao2/Classes/Personagem.cs
@@ -0,0 +1,12 @@
+namespace Personagens.Core 
+[
+    public class Char
+    [
+        public char Raca { get; set; }
+        public double Vida { get; set; }
+        public double Barra de Poder { get; set; }
+        public int[,] Inventario { get; set; } = new int[5, 5];
+        public int Arma {get; set;}
+        public var Habilidade {get; set;}
+    ]
+]
---
+++ Licao2/ConsoleApp1/Program.cs
@@ -0,0 +1,8 @@
+﻿internal class Program
+{
+    private static void Main(string[] args) 
+    { var Char = new Char();
+        Console.WriteLine("Hello, World!");
+
+    }
+} 
